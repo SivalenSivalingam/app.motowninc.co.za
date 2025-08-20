bc83af47354d7c41cc3e5de466575f0922becda8
Sivalen Sivalingam <Sivalen.Sivalingam@sage.com>
2025-08-20 23:37:35 +0200

    wip

--- app.motowninc.co.za/app.motowninc.co.za/pos/dashboard.aspx.cs
+++ app.motowninc.co.za/app.motowninc.co.za/pos/dashboard.aspx.cs
@@ -220,25 +220,7 @@ public partial class pos_dashboard : Page
 
         cart.ClearCart(Session["SessionEmployeeId"].ToString());
         LoadCart();
-        var document = new PDF().CreateInvoice(InvoiceId);
-        Dowload(document.Item1, document.Item2);
-    }
-
-    private void Dowload(Document document, string fileName)
-    {
-        var pdfRenderer = new PdfDocumentRenderer();
-        pdfRenderer.Document = document;
-        pdfRenderer.RenderDocument();
-
-        MemoryStream stream = new MemoryStream();
-        pdfRenderer.Save(stream, false);
-        Response.Clear();
-        Response.AddHeader("Content-type", "application/pdf");
-        Response.AddHeader("Content-Disposition", "attachment; filename=" + fileName + ".pdf");
-        Response.BinaryWrite(stream.ToArray());
-        Response.Flush();
-        stream.Close();
-        Response.End();
+        Response.Redirect("/pos/invoices/view?id=" + InvoiceId);
     }
 
     protected void ClearCart_Click(object sender, EventArgs e)
--- app.motowninc.co.za/app.motowninc.co.za/pos/invoices/view.aspx.cs
+++ app.motowninc.co.za/app.motowninc.co.za/pos/invoices/view.aspx.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MigraDoc.DocumentObjectModel;
+using MigraDoc.Rendering;
+using System;
+using System.IO;
 using System.Web.UI;
 
 public partial class pos_invoices_view : Page
@@ -18,6 +21,24 @@ public partial class pos_invoices_view : Page
 
     protected void Download_Click(object sender, EventArgs e)
     {
+        var document = new PDF().CreateInvoice(Request.QueryString["id"].ToString());
+        Dowload(document.Item1, document.Item2);
+    }
 
+    private void Dowload(Document document, string fileName)
+    {
+        var pdfRenderer = new PdfDocumentRenderer();
+        pdfRenderer.Document = document;
+        pdfRenderer.RenderDocument();
+
+        MemoryStream stream = new MemoryStream();
+        pdfRenderer.Save(stream, false);
+        Response.Clear();
+        Response.AddHeader("Content-type", "application/pdf");
+        Response.AddHeader("Content-Disposition", "attachment; filename=" + fileName + ".pdf");
+        Response.BinaryWrite(stream.ToArray());
+        Response.Flush();
+        stream.Close();
+        Response.End();
     }
 }
